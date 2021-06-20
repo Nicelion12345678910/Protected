14670821a6506886955f17357a8a57c0d81a1cef
Nicelion12345678910 <86024686+Nicelion12345678910@users.noreply.github.com>
2021-06-21 01:43:37 +0200

    Update accounts.lua

--- accounts.lua
+++ accounts.lua
@@ -1,3 +1,5 @@
+-- Don't try anything. Passwords are obviously encrypted.
+
 return {
-	[2630517334] = {password = "790 5014 2447 3618 1419 2066 2845 5128"} --wWmPNYnT
+	["buyver19"] = {password = "790 5014 2447 3618 1419 2066 2845 5128"}
 }
